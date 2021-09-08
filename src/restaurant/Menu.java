ccb544c851694b9d6dac0a8552abbf324fea41b7
swankidelic <swankidelic@gmail.com>
2021-09-07 23:08:40 -0500

    MenuItem clas is done, working on Menu class methods. Working on accessing MenuItem methods

--- src/restaurant/Menu.java
+++ src/restaurant/Menu.java
@@ -1,4 +1,24 @@
 package restaurant;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Menu {
+    private ArrayList<MenuItem> menuItems = new ArrayList<>();
+    private Date dateLastUpdated;
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+    public Date getDateLastUpdated() {
+        for (MenuItem item : menuItems){
+            if (dateLastUpdated.compareTo(menuItems(item).getDateAdded) < 7) {
+                dateLastUpdated = menuItems(item).getDateAdded;
+            }
+        }
+        return dateLastUpdated;
+    }
+
+
+
 }
--- src/restaurant/MenuItem.java
+++ src/restaurant/MenuItem.java
@@ -1,4 +1,32 @@
 package restaurant;
 
+import java.util.Date;
+
 public class MenuItem {
+    private String name;
+    private String category;
+    private String description;
+    private double price;
+    private Date dateAdded;
+    private boolean newItem;
+
+    public boolean createNewMenuItem(String newItemName, String newItemCategory, String newItemDesc, double newItemPrice) {
+        this.name = newItemName;
+        this.category = newItemCategory;
+        this.description = newItemDesc;
+        this.price = newItemPrice;
+        this.dateAdded = new Date();
+        return true;
+    }
+
+    public String getName(){return this.name;}
+    public String getCategory(){return this.category;}
+    public String getDesc(){return this.description;}
+    public double getPrice(){return this.price;}
+    public Date getDateAdded(){return this.dateAdded;}
+
+    public void setName(String newName){this.name = newName;}
+    public void setCategory(String newCat){this.category = newCat;}
+    public void setDesc(String newDesc){this.description = newDesc;}
+    public void setPrice(double newPrice){this.price = newPrice;}
 }
